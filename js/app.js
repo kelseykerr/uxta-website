89c501b7da3031c69ccc44f3714f84c289a3f1f2
Kelsey Kerr <kerr.kelsey@gmail.com>
2017-06-29 18:20:50 -0400

    + search through loan/sell items on website based on zip

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ angular
     'ui.router',
     /*'ui.bootstrap',*/
     'ngTouch',
-    'duScroll'
+    'duScroll',
+    'angularGrid'
   ])
 
   .config(function($stateProvider, $locationProvider) {
@@ -31,11 +32,58 @@ angular
         url: '/terms',
         templateUrl: '/views/tos.html',
         controller: 'homeCtrl'
+      })
+      .state('iventory', {
+        url: '/inventory',
+        templateUrl: '/views/inventory.html',
+        controller: 'homeCtrl'
       });
     $locationProvider.html5Mode(true);
   })
-  .controller('homeCtrl', function($scope) {
+  .controller('homeCtrl', function($scope, $http) {
     // Highlight the top nav as scrolling occurs
+    $scope.inventory = {
+      items: [],
+
+      zip: '',
+
+      notAvailable: false,
+
+      noResults: false,
+
+      getPhotoUrl: function(photo) {
+        if (photo.photos === undefined || photo.photos.length < 1) {
+          return "img/nearby_pin_blue.png";
+        } else {
+          return "https://s3.amazonaws.com/nearbyappphotos/" + photo.photos[0];
+        }
+      },
+
+      loadItems: function() {
+        $http.get('https://server.thenearbyapp.com/api/requests/public?zip=' + $scope.inventory.zip)
+          .then(function(res) {
+            $scope.inventory.notAvailable = false;
+            $scope.inventory.items = res.data;
+            if ($scope.inventory.items.length == 0) {
+              $scope.inventory.notResults = true;
+            } else {
+              $scope.inventory.notResults = true;
+            }
+          }, function(err) {
+            $scope.inventory.items = [];
+            console.log(err);
+            if (err.status == 403) {
+              $scope.inventory.notAvailable = true;
+              $scope.inventory.notResults = false;
+            } else {
+              $scope.inventory.notResults = true;
+              $scope.inventory.notAvailable = false;
+            }
+          });
+      },
+
+      init: function() {}
+    };
 
   })
   .controller('navCtrl', function($scope, $anchorScroll) {
@@ -60,4 +108,4 @@ angular
         scope.$apply();
       });
     };
-  });;
+  });
